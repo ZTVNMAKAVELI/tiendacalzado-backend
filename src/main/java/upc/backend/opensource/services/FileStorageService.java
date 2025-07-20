06bf85841e1e587e2616749b51231d50d36580d3
Steven Lazaro <sslc.247@gmail.com>
2025-07-20 01:21:23 -0500

    Cambios FileStorageService deploy GoogleCP

--- src/main/java/upc/backend/opensource/services/FileStorageService.java
+++ src/main/java/upc/backend/opensource/services/FileStorageService.java
@@ -23,23 +23,20 @@ public class FileStorageService {
 
     public String storeFile(MultipartFile file) {
         try {
-            // nombre de archivo unico
+
             String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
 
-            // informacion del objeto
             BlobId blobId = BlobId.of(bucketName, fileName);
             BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
                     .setContentType(file.getContentType())
                     .build();
 
-            // Sube el archivo al bucket de Google Cloud Storage
-            storage.create(blobInfo, file.getBytes());
-            storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+            storage.createFrom(blobInfo, file.getInputStream());
 
             return fileName;
 
         } catch (IOException ex) {
-            throw new RuntimeException("No se pudo almacenar el archivo en Google", ex);
+            throw new RuntimeException("No se pudo almacenar el archivo en Google Cloud Storage", ex);
         }
     }
 }
